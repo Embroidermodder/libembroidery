10322bf97e7a06b0c0e6dd553c343929be8f4a15
robin-swift <87496244+robin-swift@users.noreply.github.com>
2021-08-04 10:58:32 +0100

    Create libembroidery_test_suite.sh

---
+++ libembroidery-test/libembroidery_test_suite.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Builds and runs the libembroidery_convert executable (along with the
+# static and dynamic versions of the library) and tests the reading
+# of CSV input and writing of SVG output, a common use case where
+# the user wishes to know what their script created CSV looks right.
+#
+# Then it outputs machine-compatable stitch files should the dev
+# wish to run those tests on a machine.
+mkdir build
+cd build
+cmake ..
+make
+./libembroidery_convert ../libembroidery-test/test01.csv test01.svg
+./libembroidery_convert ../libembroidery-test/test02.csv test02.svg
+./libembroidery_convert ../libembroidery-test/test01.csv test01.dst
+./libembroidery_convert ../libembroidery-test/test02.csv test02.dst

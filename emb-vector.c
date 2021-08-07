ae59f11c64b66e3e3f590b65a5d79dbf61ce1f5c
robin-swift <robinswiftart@gmail.com>
2021-08-07 22:31:51 +0100

    Strengthing the EmbVector support.

--- emb-vector.c
+++ emb-vector.c
@@ -1,31 +1,110 @@
-#include "emb-vector.h"
 #include "emb-logging.h"
+#include "emb-vector.h"
 #include <math.h>
 #include <stdlib.h>
 
+/**
+ * Finds the unit length vector \a result in the same direction as \a vector.
+ */
 void embVector_normalize(EmbVector vector, EmbVector* result)
 {
     double length = embVector_getLength(vector);
 
-    if(!result) { embLog_error("emb-vector.c embVector_normalize(), result argument is null\n"); return; }
+    if (!result) {
+        embLog_error("emb-vector.c embVector_normalize(), result argument is null\n");
+        return;
+    }
     result->X = vector.X / length;
     result->Y = vector.Y / length;
 }
 
+/**
+ * The scalar multiple \a magnatude of a vector \a vector. Returned as
+ * \a result.
+ */
 void embVector_multiply(EmbVector vector, double magnitude, EmbVector* result)
 {
-    if(!result) { embLog_error("emb-vector.c embVector_multiply(), result argument is null\n"); return; }
+    if (!result) {
+        embLog_error("emb-vector.c embVector_multiply(), result argument is null\n");
+        return;
+    }
     result->X = vector.X * magnitude;
     result->Y = vector.Y * magnitude;
 }
 
+/**
+ * The sum of vectors \a v1 and \a v2 returned as \a result.
+ */
 void embVector_add(EmbVector v1, EmbVector v2, EmbVector* result)
 {
-    if(!result) { embLog_error("emb-vector.c embVector_add(), result argument is null\n"); return; }
+    if (!result) {
+        embLog_error("emb-vector.c embVector_add(), result argument is null\n");
+        return;
+    }
     result->X = v1.X + v2.X;
     result->Y = v1.Y + v2.Y;
 }
 
+/**
+ * The average of vectors \a v1 and \a v2 returned as \a result.
+ */
+void embVector_average(EmbVector v1, EmbVector v2, EmbVector* result)
+{
+    if (!result) {
+        embLog_error("emb-vector.c embVector_add(), result argument is null\n");
+        return;
+    }
+    result->X = (v1.X + v2.X) / 2.0;
+    result->Y = (v1.Y + v2.Y) / 2.0;
+}
+
+/**
+ * The difference between vectors \a v1 and \a v2 returned as \a result.
+ */
+void embVector_subtract(EmbVector v1, EmbVector v2, EmbVector* result)
+{
+    if (!result) {
+        embLog_error("emb-vector.c embVector_subtract(), result argument is null\n");
+        return;
+    }
+    result->X = v1.X - v2.X;
+    result->Y = v1.Y - v2.Y;
+}
+
+/**
+ * The dot product as vectors \a v1 and \a v2 returned as a double.
+ *
+ * That is
+ * (x)   (a) = xa+yb
+ * (y) . (b)
+ */
+double embVector_dot(EmbVector v1, EmbVector v2)
+{
+    return v1.X * v2.X + v1.Y * v2.Y;
+}
+
+/**
+ * Since we aren't using full vector algebra here, all vectors are "vertical".
+ * so this is like the product v1^{T} I_{2} v2 for our vectors \a v1 and \v2
+ * so a "component-wise product". The result is stored at the pointer \a result.
+ *
+ * That is
+ *      (1 0) (a) = (xa)
+ * (x y)(0 1) (b)   (yb)
+ */
+void embVector_transpose_product(EmbVector v1, EmbVector v2, EmbVector* result)
+{
+    if (!result) {
+        embLog_error("emb-vector.c embVector_transpose_product(), result argument is null\n");
+        return;
+    }
+    result->X = v1.X * v2.X;
+    result->Y = v1.Y * v2.Y;
+}
+
+/**
+ * The length or absolute value of the vector \a vector. 
+ */
 double embVector_getLength(EmbVector vector)
 {
     return sqrt(vector.X * vector.X + vector.Y * vector.Y);
@@ -34,7 +113,10 @@ double embVector_getLength(EmbVector vector)
 EmbVectorList* embVectorList_create(EmbVector data)
 {
     EmbVectorList* pointer = (EmbVectorList*)malloc(sizeof(EmbVectorList));
-    if(!pointer) { embLog_error("emb-vector.c embVectorList_create(), cannot allocate memory for pointer\n"); return 0; }
+    if (!pointer) {
+        embLog_error("emb-vector.c embVectorList_create(), cannot allocate memory for pointer\n");
+        return 0;
+    }
     pointer->vector = data;
     pointer->next = 0;
     return pointer;
@@ -42,10 +124,19 @@ EmbVectorList* embVectorList_create(EmbVector data)
 
 EmbVectorList* embVectorList_add(EmbVectorList* pointer, EmbVector data)
 {
-    if(!pointer) { embLog_error("emb-vector.c embVectorList_add(), pointer argument is null\n"); return 0; }
-    if(pointer->next) { embLog_error("emb-vector.c embVectorList_add(), pointer->next should be null\n"); return 0; }
+    if (!pointer) {
+        embLog_error("emb-vector.c embVectorList_add(), pointer argument is null\n");
+        return 0;
+    }
+    if (pointer->next) {
+        embLog_error("emb-vector.c embVectorList_add(), pointer->next should be null\n");
+        return 0;
+    }
     pointer->next = (EmbVectorList*)malloc(sizeof(EmbVectorList));
-    if(!pointer->next) { embLog_error("emb-vector.c embVectorList_add(), cannot allocate memory for pointer->next\n"); return 0; }
+    if (!pointer->next) {
+        embLog_error("emb-vector.c embVectorList_add(), cannot allocate memory for pointer->next\n");
+        return 0;
+    }
     pointer = pointer->next;
     pointer->vector = data;
     pointer->next = 0;
@@ -55,9 +146,9 @@ EmbVectorList* embVectorList_add(EmbVectorList* pointer, EmbVector data)
 int embVectorList_count(EmbVectorList* pointer)
 {
     int i = 1;
-    if(!pointer) return 0;
-    while(pointer->next)
-    {
+    if (!pointer)
+        return 0;
+    while (pointer->next) {
         pointer = pointer->next;
         i++;
     }
@@ -66,15 +157,14 @@ int embVectorList_count(EmbVectorList* pointer)
 
 int embVectorList_empty(EmbVectorList* pointer)
 {
-    if(!pointer)
+    if (!pointer)
         return 1;
     return 0;
 }
 
 void embVectorList_free(EmbVectorList* pointer)
 {
-    while(pointer)
-    {
+    while (pointer) {
         EmbVectorList* temp = pointer;
         pointer = pointer->next;
         free(temp);

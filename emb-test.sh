943f696ce3d17bed5ba3c9ed86ce6212936dfc67
robin-swift <87496244+robin-swift@users.noreply.github.com>
2021-09-15 09:56:22 +0100

    Update emb-test.sh

--- emb-test.sh
+++ emb-test.sh
@@ -1,47 +1,14 @@
 #!/bin/bash
 #
 # Builds and runs the libembroidery_convert executable (along with the
-# static and dynamic versions of the library) and tests the reading
-# of CSV input and writing of SVG output, a common use case where
-# the user wishes to know what their script created CSV looks right.
+# static and dynamic versions of the library) and runs the in-built
+# test suite.
 #
-# Then it outputs machine-compatable stitch files should the dev
-# wish to run those tests on a machine.
+# This matches the GitHub Action we have set up.
 
-function build_debug () {
 rm -fr build
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug .. &> test.log
 make &>> test.log
 gdb --batch -ex=r --args ./embroider -test &>> test.log
-}
-
-function valgrind_report () {
-MEMORYLEAKS=$(cat "_memleak.txt" | grep "All heap blocks were freed -- no leaks are possible")
-
-echo "=============================="
-if [[ -z "$MEMORYLEAKS" ]]; then
-    echo $(cat "_memleak.txt" | grep "LEAK SUMMARY:")
-    echo $(cat "_memleak.txt" | grep "definitely lost:")
-    echo $(cat "_memleak.txt" | grep "indirectly lost:")
-    echo $(cat "_memleak.txt" | grep "possibly lost:")
-    echo $(cat "_memleak.txt" | grep "still reachable:")
-    echo "Review _memleak.txt for more information."
-else
-    echo "No memory leaks found :D"
-fi
-echo "=============================="
-}
-
-function valgrind_embroider () {
-valgrind --leak-check=full --show-reachable=yes \
-    --error-limit=no --suppressions=../valgrind-supp/./valgrind-qt.supp \
-    --suppressions=../valgrind-supp/./valgrind-misc.supp \
-    --gen-suppressions=all --log-file=_memleak.txt -v \
-    ./embroider "$@"
-cat ./_memleak.txt | ../valgrind-supp/./valgrind-create-suppressions.sh > _memleak.supp
-valgrind_report
-}
-
-build_debug

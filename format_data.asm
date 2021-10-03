e9b87fbe59e3b219dbdfffc301b5498fdadcee2c
robin-swift <robinswiftart@gmail.com>
2021-10-03 19:40:46 +0100

    improving assembly datafile

---
+++ format_data.asm
@@ -0,0 +1,245 @@
+; ------------------------------------------------------------------------------
+;                            FILE FORMAT DATA
+; ------------------------------------------------------------------------------
+
+%macro	csv_comment	1
+
+	db	34, "#", 34, ", ", 34, %1, 34, 10
+
+%endmacro
+
+; ------------------------------------------------------------------------------
+; TODO: This list needs reviewed in case some stitch formats
+; also can contain object data (EMBFORMAT_STCHANDOBJ).
+
+%define STITCHONLY	1
+%define OBJECTONLY	2
+%define STCHANDOBJ	2
+
+%macro	format_entry	5
+
+%%startofext:
+	db	%1
+%%endofext:
+	times	6 + %%startofext - %%endofext db 0
+
+%%startofstr:
+	db	%2
+%%endofstr:
+	times	50 + %%startofstr - %%endofstr db 0
+
+	db	%3, %4, %5
+
+%endmacro
+
+; ------------------------------------------------------------------------------
+
+formatTable:
+	format_entry	".10o", "Toyota Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".100", "Toyota Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".art", "Bernina Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".bmc", "Bitmap Cache Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".bro", "Bits & Volts Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".cnd", "Melco Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".col", "Embroidery Thread Color Format", 'U', 'U', STITCHONLY
+	format_entry	".csd", "Singer Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".csv", "Comma Separated Values Format", 'U', 'U', STITCHONLY
+	format_entry	".dat", "Barudan Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".dem", "Melco Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".dsb", "Barudan Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".dst", "Tajima Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".dsz", "ZSK USA Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".dxf", "Drawing Exchange Format", ' ', ' ', OBJECTONLY
+	format_entry	".edr", "Embird Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".emd", "Elna Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".exp", "Melco Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".exy", "Eltac Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".eys", "Sierra Expanded Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".fxy", "Fortron Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".gc", "Smoothie G-Code Format", ' ', ' ', STITCHONLY
+	format_entry	".gnc", "Great Notions Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".gt", "Gold Thread Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".hus", "Husqvarna Viking Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".inb", "Inbro Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".inf", "Embroidery Color Format", 'U', 'U', STITCHONLY
+	format_entry	".jef", "Janome Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".ksm", "Pfaff Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".max", "Pfaff Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".mit", "Mitsubishi Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".new", "Ameco Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".ofm", "Melco Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".pcd", "Pfaff Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".pcm", "Pfaff Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".pcq", "Pfaff Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".pcs", "Pfaff Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".pec", "Brother Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".pel", "Brother Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".pem", "Brother Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".pes", "Brother Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".phb", "Brother Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".phc", "Brother Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".plt", "AutoCAD Plot Drawing Format", 'U', 'U', STITCHONLY
+	format_entry	".rgb", "RGB Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".sew", "Janome Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".shv", "Husqvarna Viking Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".sst", "Sunstar Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".stx", "Data Stitch Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".svg", "Scalable Vector Graphics", 'U', 'U', OBJECTONLY
+	format_entry	".t01", "Pfaff Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".t09", "Pfaff Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".tap", "Happy Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".thr", "ThredWorks Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".txt", "Text File", ' ', 'U', STITCHONLY
+	format_entry	".u00", "Barudan Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".u01", "Barudan Embroidery Format", ' ', ' ', STITCHONLY
+	format_entry	".vip", "Pfaff Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	".vp3", "Pfaff Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".xxx", "Singer Embroidery Format", 'U', 'U', STITCHONLY
+	format_entry	".zsk", "ZSK USA Embroidery Format", 'U', ' ', STITCHONLY
+	format_entry	"END", "END", ' ', ' ', 0
+
+
+stitchTypeLabels:
+	dd	stitch_token
+	dd	jump_token
+	dd	trim_token
+	dd	color_token
+	dd	end_token
+	dd	unknown_token
+	dd	0
+
+stitch_token:
+	db	"STITCH", 0
+jump_token:
+	db	"JUMP", 0
+trim_token:
+	db	"TRIM", 0
+color_token:
+	db	"COLOR", 0
+end_token:
+	db	"END", 0
+unknown_token:
+	db	"UNKNOWN", 0
+
+
+csvHeader:
+	csv_comment	"Embroidermodder 2 CSV Embroidery File"
+	csv_comment	"http://embroidermodder.github.io"
+	db	10
+	csv_comment	"General Notes:"
+	csv_comment	"This file can be read by Excel or LibreOffice as CSV (Comma Separated Value) or with a text editor."
+	csv_comment	"Lines beginning with # are comments."
+	csv_comment	"Lines beginning with > are variables: [VAR_NAME], [VAR_VALUE]"
+	csv_comment	"Lines beginning with $ are threads: [THREAD_NUMBER], [RED], [GREEN], [BLUE], [DESCRIPTION], [CATALOG_NUMBER]"
+	csv_comment	"Lines beginning with * are stitch entries: [STITCH_TYPE], [X], [Y]"
+	db	10
+	csv_comment	"Stitch Entry Notes:"
+	csv_comment	"STITCH instructs the machine to move to the position [X][Y] and then make a stitch."
+	csv_comment	"JUMP instructs the machine to move to the position [X][Y] without making a stitch."
+	csv_comment	"TRIM instructs the machine to cut the thread before moving to the position [X][Y] without making a stitch."
+	csv_comment	"COLOR instructs the machine to stop temporarily so that the user can change to a different color thread before resuming."
+	csv_comment	"END instructs the machine that the design is completed and there are no further instructions."
+	csv_comment	"UNKNOWN encompasses instructions that may not be supported currently."
+	csv_comment	"[X] and [Y] are absolute coordinates in millimeters (mm)."
+	db	10, 0
+
+
+vipDecodingTable:
+	db	0x2E, 0x82, 0xE4, 0x6F, 0x38, 0xA9, 0xDC, 0xC6, 0x7B, 0xB6, 0x28, 0xAC, 0xFD, 0xAA, 0x8A, 0x4E
+	db	0x76, 0x2E, 0xF0, 0xE4, 0x25, 0x1B, 0x8A, 0x68, 0x4E, 0x92, 0xB9, 0xB4, 0x95, 0xF0, 0x3E, 0xEF
+	db	0xF7, 0x40, 0x24, 0x18, 0x39, 0x31, 0xBB, 0xE1, 0x53, 0xA8, 0x1F, 0xB1, 0x3A, 0x07, 0xFB, 0xCB
+	db	0xE6, 0x00, 0x81, 0x50, 0x0E, 0x40, 0xE1, 0x2C, 0x73, 0x50, 0x0D, 0x91, 0xD6, 0x0A, 0x5D, 0xD6
+	db	0x8B, 0xB8, 0x62, 0xAE, 0x47, 0x00, 0x53, 0x5A, 0xB7, 0x80, 0xAA, 0x28, 0xF7, 0x5D, 0x70, 0x5E
+	db	0x2C, 0x0B, 0x98, 0xE3, 0xA0, 0x98, 0x60, 0x47, 0x89, 0x9B, 0x82, 0xFB, 0x40, 0xC9, 0xB4, 0x00
+	db	0x0E, 0x68, 0x6A, 0x1E, 0x09, 0x85, 0xC0, 0x53, 0x81, 0xD1, 0x98, 0x89, 0xAF, 0xE8, 0x85, 0x4F
+	db	0xE3, 0x69, 0x89, 0x03, 0xA1, 0x2E, 0x8F, 0xCF, 0xED, 0x91, 0x9F, 0x58, 0x1E, 0xD6, 0x84, 0x3C
+	db	0x09, 0x27, 0xBD, 0xF4, 0xC3, 0x90, 0xC0, 0x51, 0x1B, 0x2B, 0x63, 0xBC, 0xB9, 0x3D, 0x40, 0x4D
+	db	0x62, 0x6F, 0xE0, 0x8C, 0xF5, 0x5D, 0x08, 0xFD, 0x3D, 0x50, 0x36, 0xD7, 0xC9, 0xC9, 0x43, 0xE4
+	db	0x2D, 0xCB, 0x95, 0xB6, 0xF4, 0x0D, 0xEA, 0xC2, 0xFD, 0x66, 0x3F, 0x5E, 0xBD, 0x69, 0x06, 0x2A
+	db	0x03, 0x19, 0x47, 0x2B, 0xDF, 0x38, 0xEA, 0x4F, 0x80, 0x49, 0x95, 0xB2, 0xD6, 0xF9, 0x9A, 0x75
+	db	0xF4, 0xD8, 0x9B, 0x1D, 0xB0, 0xA4, 0x69, 0xDB, 0xA9, 0x21, 0x79, 0x6F, 0xD8, 0xDE, 0x33, 0xFE
+	db	0x9F, 0x04, 0xE5, 0x9A, 0x6B, 0x9B, 0x73, 0x83, 0x62, 0x7C, 0xB9, 0x66, 0x76, 0xF2, 0x5B, 0xC9
+	db	0x5E, 0xFC, 0x74, 0xAA, 0x6C, 0xF1, 0xCD, 0x93, 0xCE, 0xE9, 0x80, 0x53, 0x03, 0x3B, 0x97, 0x4B
+	db	0x39, 0x76, 0xC2, 0xC1, 0x56, 0xCB, 0x70, 0xFD, 0x3B, 0x3E, 0x52, 0x57, 0x81, 0x5D, 0x56, 0x8D
+	db	0x51, 0x90, 0xD4, 0x76, 0xD7, 0xD5, 0x16, 0x02, 0x6D, 0xF2, 0x4D, 0xE1, 0x0E, 0x96, 0x4F, 0xA1
+	db	0x3A, 0xA0, 0x60, 0x59, 0x64, 0x04, 0x1A, 0xE4, 0x67, 0xB6, 0xED, 0x3F, 0x74, 0x20, 0x55, 0x1F
+	db	0xFB, 0x23, 0x92, 0x91, 0x53, 0xC8, 0x65, 0xAB, 0x9D, 0x51, 0xD6, 0x73, 0xDE, 0x01, 0xB1, 0x80
+	db	0xB7, 0xC0, 0xD6, 0x80, 0x1C, 0x2E, 0x3C, 0x83, 0x63, 0xEE, 0xBC, 0x33, 0x25, 0xE2, 0x0E, 0x7A
+	db	0x67, 0xDE, 0x3F, 0x71, 0x14, 0x49, 0x9C, 0x92, 0x93, 0x0D, 0x26, 0x9A, 0x0E, 0xDA, 0xED, 0x6F
+	db	0xA4, 0x89, 0x0C, 0x1B, 0xF0, 0xA1, 0xDF, 0xE1, 0x9E, 0x3C, 0x04, 0x78, 0xE4, 0xAB, 0x6D, 0xFF
+	db	0x9C, 0xAF, 0xCA, 0xC7, 0x88, 0x17, 0x9C, 0xE5, 0xB7, 0x33, 0x6D, 0xDC, 0xED, 0x8F, 0x6C, 0x18
+	db	0x1D, 0x71, 0x06, 0xB1, 0xC5, 0xE2, 0xCF, 0x13, 0x77, 0x81, 0xC5, 0xB7, 0x0A, 0x14, 0x0A, 0x6B
+	db	0x40, 0x26, 0xA0, 0x88, 0xD1, 0x62, 0x6A, 0xB3, 0x50, 0x12, 0xB9, 0x9B, 0xB5, 0x83, 0x9B, 0x37
+
+
+maxHeader:
+	db	0x56, 0x43, 0x53, 0x4D, 0xFC, 0x03, 0x00, 0x00
+	db	0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00
+	db	0xF6, 0x25, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x08, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x31, 0x33, 0x37, 0x38
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x4D, 0x61, 0x64, 0x65, 0x69, 0x72, 0x61, 0x20
+	db	0x52, 0x61, 0x79, 0x6F, 0x6E, 0x20, 0x34, 0x30
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+	db	0x01, 0x38, 0x09, 0x31, 0x33, 0x30, 0x2F, 0x37
+	db	0x30, 0x35, 0x20, 0x48, 0xFA, 0x00, 0x00, 0x00
+	db	0x00, 0x00, 0x00, 0x00, 0x00
+
+
+imageWithFrame:
+	db	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	db	0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0
+	db	0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0
+	db	0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
+	db	0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0
+	db	0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0
+	db	0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0
+	db	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+
---
+++ string_table.asm
@@ -0,0 +1,490 @@
+;-------------------------------------------------------------------------------
+;	String table
+;-------------------------------------------------------------------------------
+
+audio_level_token:
+	db	"audio-level", 0
+buffered_rendering_token:
+	db	"buffered-rendering", 0
+color_token:
+	db	"color", 0
+color_rendering_token:
+	db	"color-rendering", 0
+direction_token:
+	db	"direction", 0
+display_token:
+	db	"display", 0
+display_align_token:
+	db	"display-align", 0
+fill_token:
+	db	"fill", 0
+fill_opacity_token:
+	db	"fill-opacity", 0
+fill_rule_token:
+	db	"fill-rule", 0
+font_family_token:
+	db	"font-family", 0
+font_size_token:
+	db	"font-size", 0
+font_style_token:
+	db	"font-style", 0
+font_variant_token:
+	db	"font-variant", 0
+font_weight_token:
+	db	"font-weight", 0
+image_rendering_token:
+	db	"image-rendering", 0
+line_increment_token:
+	db	"line-increment", 0
+opacity_token:
+	db	"opacity", 0
+pointer_events_token:
+	db	"pointer-events", 0
+shape_rendering_token:
+	db	"shape-rendering", 0
+solid_color_token:
+	db	"solid-color", 0
+solid_opacity_token:
+	db	"solid-opacity", 0
+stop_color_token:
+	db	"stop-color", 0
+stop_opacity_token:
+	db	"stop-opacity", 0
+stroke_token:
+	db	"stroke", 0
+stroke_dasharray_token:
+	db	"stroke-dasharray", 0
+stroke_linecap_token:
+	db	"stroke-linecap", 0
+stroke_linejoin_token:
+	db	"stroke-linejoin", 0
+stroke_miterlimit_token:
+	db	"stroke-miterlimit", 0
+stroke_opacity_token:
+	db	"stroke-opacity", 0
+stroke_width_token:
+	db	"stroke-width", 0
+text_align_token:
+	db	"text-align", 0
+text_anchor_token:
+	db	"text-anchor", 0
+text_rendering_token:
+	db	"text-rendering", 0
+unicode_bidi_token:
+	db	"unicode-bidi", 0
+vector_effect_token:
+	db	"vector-effect", 0
+viewport_fill_token:
+	db	"viewport-fill", 0
+viewport_fill_opacity_token:
+	db	"viewport-fill-opacity", 0
+visibility_token:
+	db	"visibility", 0
+about_token:
+	db	"about", 0
+accent_height_token:
+	db	"accent-height", 0
+accumulate_token:
+	db	"accumulate", 0
+additive_token:
+	db	"additive", 0
+alphabetic_token:
+	db	"alphabetic", 0
+arabic_form_token:
+	db	"arabic-form", 0
+ascent_token:
+	db	"ascent", 0
+attribute_name_token:
+	db	"attributeName", 0
+attribute_type_token:
+	db	"attributeType", 0
+bandwidth_token:
+	db	"bandwidth", 0
+base_profile_token:
+	db	"baseProfile", 0
+bbox_token:
+	db	"bbox", 0
+begin_token:
+	db	"begin", 0
+by_token:
+	db	"by", 0
+calc_mode_token:
+	db	"calcMode", 0
+cap_height_token:
+	db	"cap-height", 0
+class_token:
+	db	"class", 0
+content_token:
+	db	"content", 0
+content_script_type_token:
+	db	"contentScriptType", 0
+cx_token:
+	db	"cx", 0
+cy_token:
+	db	"cy", 0
+d_token:
+	db	"d", 0
+datatype_token:
+	db	"datatype", 0
+default_action_token:
+	db	"defaultAction", 0
+descent_token:
+	db	"descent", 0
+dur_token:
+	db	"dur", 0
+editable_token:
+	db	"editable", 0
+end_token:
+	db	"end", 0
+ev_event_token:
+	db	"ev:event", 0
+event_token:
+	db	"event", 0
+external_resources_required_token:
+	db	"externalResourcesRequired", 0
+focus_highlight_token:
+	db	"focusHighlight", 0
+focusable_token:
+	db	"focusable", 0
+font_stretch_token:
+	db	"font-stretch", 0
+from_token:
+	db	"from", 0
+g1_token:
+	db	"g1", 0
+g2_token:
+	db	"g2", 0
+glyph_name_token:
+	db	"glyph-name", 0
+gradient_units_token:
+	db	"gradientUnits", 0
+handler_token:
+	db	"handler", 0
+hanging_token:
+	db	"hanging", 0
+height_token:
+	db	"height", 0
+horiz_adv_x_token:
+	db	"horiz-adv-x", 0
+horiz_origin_x_token:
+	db	"horiz-origin-x", 0
+id_token:
+	db	"id", 0
+ideographic_token:
+	db	"ideographic", 0
+initial_visibility_token:
+	db	"initialVisibility", 0
+k_token:
+	db	"k", 0
+key_points_token:
+	db	"keyPoints", 0
+key_splines_token:
+	db	"keySplines", 0
+key_times_token:
+	db	"keyTimes", 0
+lang_token:
+	db	"lang", 0
+mathematical_token:
+	db	"mathematical", 0
+max_token:
+	db	"max", 0
+media_character_encoding_token:
+	db	"mediaCharacterEncoding", 0
+media_content_encoding_token:
+	db	"mediaContentEncodings", 0
+media_size_token:
+	db	"mediaSize", 0
+media_time_token:
+	db	"mediaTime", 0
+min_token:
+	db	"min", 0
+nav_down_token:
+	db	"nav-down", 0
+nav_down_left_token:
+	db	"nav-down-left", 0
+nav_down_right_token:
+	db	"nav-down-right", 0
+nav_left_token:
+	db	"nav-left", 0
+nav_next_token:
+	db	"nav-next", 0
+nav_prev_token:
+	db	"nav-prev", 0
+nav_right_token:
+	db	"nav-right", 0
+nav_up_token:
+	db	"nav-up", 0
+nav_up_left_token:
+	db	"nav-up-left", 0
+nav_up_right_token:
+	db	"nav-up-right", 0
+observer_token:
+	db	"observer", 0
+offset_token:
+	db	"offset", 0
+origin_token:
+	db	"origin", 0
+overlay_token:
+	db	"overlay", 0
+overline_position_token:
+	db	"overline-position", 0
+overline_thickness_token:
+	db	"overline-thickness", 0
+panose_1_token:
+	db	"panose-1", 0
+path_token:
+	db	"path", 0
+path_length_token:
+	db	"pathLength", 0
+phase_token:
+	db	"phase", 0
+playback_order_token:
+	db	"playbackOrder", 0
+points_token:
+	db	"points", 0
+preserve_aspect_ratio_token:
+	db	"preserveAspectRatio", 0
+propagate_token:
+	db	"propagate", 0
+property_token:
+	db	"property", 0
+r_token:
+	db	"r", 0
+rel_token:
+	db	"rel", 0
+repeat_count_token:
+	db	"repeatCount", 0
+repeat_dur_token:
+	db	"repeatDur", 0
+required_extensions_token:
+	db	"requiredExtensions", 0
+required_features_token:
+	db	"requiredFeatures", 0
+required_fonts_token:
+	db	"requiredFonts", 0
+required_formats_token:
+	db	"requiredFormats", 0
+resource_token:
+	db	"resource", 0
+restart_token:
+	db	"restart", 0
+rev_token:
+	db	"rev", 0
+role_token:
+	db	"role", 0
+rotate_token:
+	db	"rotate", 0
+rx_token:
+	db	"rx", 0
+ry_token:
+	db	"ry", 0
+slope_token:
+	db	"slope", 0
+snapshot_time_token:
+	db	"snapshotTime", 0
+stemh_token:
+	db	"stemh", 0
+stemv_token:
+	db	"stemv", 0
+strikethrough_position_token:
+	db	"strikethrough-position", 0
+strikethrough_thickness_token:
+	db	"strikethrough-thickness", 0
+sync_behavior_token:
+	db	"syncBehavior", 0
+sync_behavior_default_token:
+	db	"syncBehaviorDefault", 0
+sync_master_token:
+	db	"syncMaster", 0
+sync_tolerance_token:
+	db	"syncTolerance", 0
+sync_tolerance_default_token:
+	db	"syncToleranceDefault", 0
+system_language_token:
+	db	"systemLanguage", 0
+target_token:
+	db	"target", 0
+timeline_begin_token:
+	db	"timelineBegin", 0
+to_token:
+	db	"to", 0
+transform_token:
+	db	"transform", 0
+transform_behavior_token:
+	db	"transformBehavior", 0
+type_token:
+	db	"type", 0
+typeof_token:
+	db	"typeof", 0
+u1_token:
+	db	"u1", 0
+u2_token:
+	db	"u2", 0
+underline_position_token:
+	db	"underline-position", 0
+underline_thickness_token:
+	db	"underline-thickness", 0
+unicode_token:
+	db	"unicode", 0
+unicode_range_token:
+	db	"unicode-range", 0
+units_per_em_token:
+	db	"units-per-em", 0
+values_token:
+	db	"values", 0
+version_token:
+	db	"version", 0
+viewbox_token:
+	db	"viewBox", 0
+width_token:
+	db	"width", 0
+widths_token:
+	db	"widths", 0
+x_token:
+	db	"x", 0
+x_height_token:
+	db	"x-height", 0
+x1_token:
+	db	"x1", 0
+x2_token:
+	db	"x2", 0
+xlink_actuate_token:
+	db	"xlink:actuate", 0
+xlink_arcrole_token:
+	db	"xlink:arcrole",
+xlink_href_token:
+	db	"xlink:href", 0
+xlink_role_token:
+	db	"xlink:role", 0
+xlink_show_token:
+	db	"xlink:show", 0
+xlink_title_token:
+	db	"xlink:title", 0
+xlink_type_token:
+	db	"xlink:type",
+xml_base_token:
+	db	"xml:base", 0
+xml_id_token:
+	db	"xml:id", 0
+xml_lang_token:
+	db	"xml:lang", 0
+xml_space_token:
+	db	"xml:space", 0
+y_token:
+	db	"y", 0
+y1_token:
+	db	"y1", 0
+y2_token:
+	db	"y2", 0
+zoom_and_pan_token:
+	db	"zoomAndPan", 0
+slash_token:
+	db	"/", 0
+
+; svg elements
+xml_token:
+	db	"?xml", 0
+a_token:
+	db	"a", 0
+animate_token:
+	db	"animate", 0
+animate_color_token:
+	db	"animateColor", 0
+animate_motion_token:
+	db	"animateMotion", 0
+animate_transform_token:
+	db	"animateTransform", 0
+animation_token:
+	db	"animation", 0
+audio_token:
+	db	"audio", 0
+circle_token:
+	db	"circle", 0
+defs_token:
+	db	"defs", 0
+desc_token:
+	db	"desc", 0
+discard_token:
+	db	"discard", 0
+ellipse_token:
+	db	"ellipse", 0
+font_token:
+	db	"font", 0
+font_face_token:
+	db	"fontFace", 0
+font_face_src_token:
+	db	"fontFaceSrc", 0
+font_face_uri_token:
+	db	"fontFaceUri", 0
+foreign_object_token:
+	db	"foreign", 0
+g_token:
+	db	"g", 0
+glyph_token:
+	db	"glyph", 0
+hkern_token:
+	db	"hkern", 0
+image_token:
+	db	"image", 0
+line_token:
+	db	"line", 0
+linear_gradient_token:
+	db	"linearGradient", 0
+listener_token:
+	db	"listener", 0
+metadata_token:
+	db	"metadata", 0
+missing_glyph_token:
+	db	"missingGlyph", 0
+mpath_token:
+	db	"mpath", 0
+polygon_token:
+	db	"polygon", 0
+polyline_token:
+	db	"polyline", 0
+prefetch_token:
+	db	"prefetch", 0
+radial_gradient_token:
+	db	"radialGradient", 0
+rect_token:
+	db	"rect", 0
+script_token:
+	db	"script", 0
+set_token:
+	db	"set", 0
+stop_token:
+	db	"stop", 0
+svg_token:
+	db	"svg", 0
+switch_token:
+	db	"switch", 0
+tbreak_token:
+	db	"tbreak", 0
+text_token:
+	db	"text", 0
+text_area_token:
+	db	"textArea", 0
+title_token:
+	db	"title", 0
+tspan_token:
+	db	"tspan", 0
+use_token:
+	db	"use", 0
+video_token:
+	db	"video", 0
+
+encoding_token:
+	db	"encoding", 0
+standalone_token:
+	db	"standalone", 0
+xmlns_token:
+	db	"xmlns", 0
+xmlns_svg_token:
+	db	"xmlns:svg", 0
+xmlns_rdf_token:
+	db	"xmlns:rdf", 0
+xmlns_dc_token:
+	db	"xmlns:dc", 0
+xmlns_cc_token:
+	db	"xmlns:cc", 0
+

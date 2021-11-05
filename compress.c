f55403e23b922401059e9cef02d01bdd612199d1
Robin Swift <87496244+robin-swift@users.noreply.github.com>
2021-11-05 12:19:36 +0000

    Reminder to rebuild the compression algorithm

---
+++ compress.c
@@ -0,0 +1,30 @@
+/*
+ *  Thanks to Jason Weiler for describing the binary formats of the HUS and
+ *  VIP formats at:
+ *  
+ *  http://www.jasonweiler.com/HUSandVIPFileFormatInfo.html
+ *
+ *  Further thanks to github user tatarize for solving the mystery of the
+ *  compression in:
+ *  
+ *  https://github.com/EmbroidePy/pyembroidery
+ *  
+ *  with a description of that work here:
+ *  
+ *  https://stackoverflow.com/questions/7852670/greenleaf-archive-library
+ *
+ *  This is based on their work.
+ */
+ 
+ #include "embroidery.h"
+ 
+ int hus_compress(char *data, int length, char *output, int *output_length)
+ {
+     return 0;
+ }
+ 
+ int hus_decompress(char *data, int length, char *output, int *output_length)
+ {
+     return 0; 
+ }
+ 

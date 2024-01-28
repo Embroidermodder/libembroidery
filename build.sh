c6543cfdbb3b93b769f209193a5dd4b9ad4f0c34
Robin Swift <robinswiftart@gmail.com>
2024-01-28 15:20:08 +0000

    collating data

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+MODE="-halt-on-error"
+#MODE="-interaction=nonstopmode"
+
+DATE=`date -I`
+A_STUB="emrm_${DATE}_a4"
+US_STUB="emrm_${DATE}_us_letter"
+
 function docs () {
 
     cd docs
@@ -13,16 +20,41 @@ function docs () {
 
 }
 
+function latex_build () {
+    cat refman.tex >> $1.tex
+
+    pdflatex $MODE $1.tex || exit 1
+#    bibtex $1.aux
+    pdflatex $MODE $1.tex
+    pdflatex $MODE $1.tex
+    pdflatex $MODE $1.tex
+}
+
 function build_refman () {
 
 	sudo apt-get update &> deps.log
 	sudo apt-get upgrade &>> deps.log
-	sudo apt-get install texlive-full &>> deps.log
+    sudo apt-get install texlive texlive-latex-extra &>> deps.log
+
+#   mandoc -c -O width=80 -T ascii docs/embroider.1 > docs/embroider.txt || exit 1
 
     cd docs/emrm
-	pdflatex refman.tex
-	cd ../..
 
+    echo "Building Reference Manual..."
+
+#   python3 make_tables.py || exit 1
+
+    printf "\\documentclass[a4paper]{report}\n\n" > ${A_STUB}.tex || exit 1
+    printf "\\documentclass[letterpaper]{report}\n\n" > ${US_STUB}.tex || exit 1
+    
+    latex_build ${A_STUB} || exit 1
+    latex_build ${US_STUB} || exit 1
+
+    tar cf emrm.tar ${A_STUB}.pdf ${US_STUB}.pdf
+
+    echo "Reference Manual successfully built in A4 and US Letter paper."
+
+	cd ../..
 }
 
 function test_fills () {

9f71862aaf09c974b5863384004237a5db39ad53
Robin Swift <87496244+robin-swift@users.noreply.github.com>
2024-03-25 00:45:41 +0000

    Delete bin directory

--- bin/build_em_on_windows.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pacman -S gcc cmake git bash mingw-w64-SDL2 \
-	mingw-w64-SDL2_image mingw-w64-SDL2_ttf
-git clone https://github.com/Embroidermodder/Embroidermodder
-cd Embroidermodder
-bash build.sh
